ada87b95938dd8e9c4862b25f217995d525f4753
Deciptikon <deciptikon@mail.ru>
2023-12-02 20:53:37 +0300

    basic version

--- main.py
+++ main.py
@@ -1,19 +1,27 @@
 import tkinter as tk
 
+VERSION = '0.1.0'
+
 def on_button_click():
     print("Кнопка была нажата!")
 
 # Создаем главное окно
 root = tk.Tk()
-root.title("Пример с кнопкой")
+root.title("Chiness Trainer" + VERSION)
+
+font_big = ("Arial", 56)  
+font_small = ("Arial", 18)  
 
 # Задаем размеры окна (ширина x высота + X + Y)
-root.geometry("400x300+100+100")
+root.geometry("1000x600+200+100")
+
+label_text = tk.Label(root, text="Текст 著名", font=font_big)
+label_text.pack(pady=20)
+
 
 # Создаем кнопку и привязываем к ней функцию on_button_click
-button = tk.Button(root, text="Нажми меня!", command=on_button_click)
-#button.pack(pady=20)
-button.pack(side="top", padx=10, pady=10)  # Пример настройки положения кнопки
+button = tk.Button(root, text="Кнопка 著名", command=on_button_click, font=font_small)
+button.pack(side="top", padx=100, pady=100)  # Пример настройки положения кнопки
 
 
 # Запускаем цикл обработки событий
