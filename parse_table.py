b685289b9cce40debac78b45e2a49763fb8b6ee7
Deciptikon <deciptikon@mail.ru>
2023-12-04 18:55:19 +0300

    ...

--- parse_table.py
+++ parse_table.py
@@ -1,5 +1,9 @@
 import pandas as pd
 
+# Парсит таблицу собирая словарь из китайских символов 
+# и их русского перевода
+
+
 path_to_file = 'book1000.xlsx'
 
 table_from_file = pd.read_excel(path_to_file)
@@ -13,11 +17,6 @@ true_symbols = {'1', '2', '3', '4', '5', '6', '7', '8', '9', '0',
                 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т',
                 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь',
                 'э', 'ю', 'я'}
-
-print("Первые несколько строк таблицы:")
-print(table_from_file.head())
-
-print(len(table_from_file))
 
 string = ''
 for i in range(3, len(table_from_file)-1):#len(table_from_file)-1
